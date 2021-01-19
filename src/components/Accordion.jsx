0d9cee17a5c9d5e947aa01820d0565e6409a5c0f
Hamza Hayd <dado.org@hotmail.com>
2021-01-19 09:12:09 +0100

    added ui-semantic link for styling

--- src/components/Accordion.jsx
+++ src/components/Accordion.jsx
@@ -1,7 +1,23 @@
+
+
 const Accordion = ({ items }) => {
+
+    const renderedItems = items.map((item) => {
+      return (
+        <div key={item.title}>
+          <div className="title active">
+            <i className="dropdown icon"></i>
+            {item.title}
+              </div>
+              <div className="content active">
+                  <p> {item.content} </p>
+              </div>
+        </div>
+      );
+    });
   return (
-    <div>
-      <h1>{items.length}</h1>
+    <div className='ui styled accordion'>
+      {renderedItems}
     </div>
   );
 };
