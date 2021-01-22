8ca49938787e4967c84fbf3c489f652d5489bba0
Hamza Hayd <dado.org@hotmail.com>
2021-01-22 12:29:20 +0100

    fixed the useEffect warning

--- src/components/Search.js
+++ src/components/Search.js
@@ -2,16 +2,21 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 const Search = () => {
-  const [term, setTerm] = useState("");
+  const [term, setTerm] = useState("programming");
+
+  const [debouncedTerm, setDebouncedTerm] = useState(term);
 
   const [results, setResults] = useState([]);
 
-  /**
-   * You're not allowed to use async-await inside
-   * the useeffect hook.
-   * * There are other ways to do this.. Like declareing a helper function
-   * * This is they React want to you to do...
-   */
+  useEffect(() => {
+    const timerId = setTimeout(() => {
+      setDebouncedTerm(term);
+    }, 1000);
+    return () => {
+      clearTimeout(timerId);
+    };
+  }, [term]);
+
   useEffect(() => {
     const search = async () => {
       const { data } = await axios.get("https://en.wikipedia.org/w/api.php", {
@@ -20,31 +25,53 @@ const Search = () => {
           list: "search",
           origin: "*",
           format: "json",
-          srsearch: term,
+          srsearch: debouncedTerm,
         },
       });
       setResults(data.query.search);
     };
-
-
-    if (term && !results.length) {
-      search();
-    } else { 
-      const timeoutId = setTimeout(() => {
-            if (term) {
-              search();
-            }
-          }, 1000);
-        /**
-         * * Clean up function
-         *
-         */
-        return () => {
-          clearTimeout(timeoutId);
-        };
-    }
+    search()
     
-  }, [term]);
+  }, [debouncedTerm]);
+
+  /**
+   * You're not allowed to use async-await inside
+   * the useeffect hook.
+   * * There are other ways to do this.. Like declareing a helper function
+   * * This is they React want to you to do...
+   */
+  // useEffect(() => {
+  //   const search = async () => {
+  //     const { data } = await axios.get("https://en.wikipedia.org/w/api.php", {
+  //       params: {
+  //         action: "query",
+  //         list: "search",
+  //         origin: "*",
+  //         format: "json",
+  //         srsearch: term,
+  //       },
+  //     });
+  //     setResults(data.query.search);
+  //   };
+
+
+  //   if (term && !results.length) {
+  //     search();
+  //   } else {
+  //     const timeoutId = setTimeout(() => {
+  //       if (term) {
+  //         search();
+  //       }
+  //     }, 1000);
+  //     /**
+  //      * * Clean up function
+  //      */
+  //     return () => {
+  //       clearTimeout(timeoutId);
+  //     };
+  //   }
+    
+  // }, [term]);
 
   /**
    * ! {dangerouslySetInnerHTML}
