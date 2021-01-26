22ac894804f7557518de649c0d39e0a82162c02f
Hamza Hayd <dado.org@hotmail.com>
2021-01-26 07:57:19 +0100

    Displaying Translated Text

--- src/components/Convert.js
+++ src/components/Convert.js
@@ -3,23 +3,31 @@ import axios from "axios";
 
 const Convert = ({ language, text }) => {
 
+   const [translated, setTranslated] = useState("");
 
     useEffect(() => {
-        axios.post(
-          "https://translation.googleapis.com/language/translate/v2",
-          {},
-          {
-            params: {
-              q: text,
-              target: language.value,
-              key: "AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM",
-            },
-          }
-        );
-    //console.log("new Language and text");
-  }, [language, text]);
 
-  return <div></div>;
+        const doTranslation = async () => {
+            const { data } = await axios.post(
+              "https://translation.googleapis.com/language/translate/v2",
+              {},
+              {
+                params: {
+                  q: text,
+                  target: language.value,
+                  key: "AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM",
+                },
+              }
+            );
+            setTranslated(data.data.translations[0].translatedText)
+        }
+
+        doTranslation()
+    },[language, text]);
+    
+  return <div>
+      <h1 className='ui header'> {translated} </h1>
+  </div>;
 };
 
 export default Convert;
